fc9c0473de974d2f4767a9a469fec27c7de080f8
taku-Matsunaga <skyline199306@yahoo.co.jp>
2022-11-25 20:48:52 +0900

    カメラ実装

--- pages/ninpu/record/capture.jsx
+++ pages/ninpu/record/capture.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 import Layout from "../../../components/layout";
 
 const Capture = () => {
+  const inputRef = useRef();
+  const handleClick = () => inputRef.current.click();
+
   return (
     <Layout>
-      <div>Capture</div>
-      <input type="file" id="user" capture="user" accept="image/*" />
+      <div className="flex items-center justify-center">
+        <button
+          className="bg-main-orange rounded-lg flex flex-col items-center p-4 aspect-square w-40 justify-center shadow-md text-white"
+          onClick={handleClick}
+        >
+          <span class="material-symbols-outlined text-white text-4xl pb-4">
+            photo_camera
+          </span>
+          カルテを撮影
+        </button>
+      </div>
       <input
         type="file"
         id="environment"
         capture="environment"
-        accept="video/*"
+        accept="image/*"
+        ref={inputRef}
+        className="hidden"
       />
     </Layout>
   );
